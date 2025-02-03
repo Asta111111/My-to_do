04dd89946933f2b565e0750413c52d18739c360b
asta111111 <bohdanbobkov202@gmail.com>
2025-02-03 21:54:23 +0100

    add processing text and + id for text

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect
+import random
 
 app = Flask(__name__)
 
@@ -13,9 +14,9 @@ def index():
         if request.form.get('action1') == 'VALUE1':
             return redirect("/add", code=302)
         elif  request.form.get('action2') == 'VALUE2':
-            pass # do something else
+            pass 
         else:
-            pass # unknown
+            pass 
     elif request.method == 'GET':
         return render_template('index.html')
     
@@ -23,15 +24,24 @@ def index():
 
 
 
+savetext = {}
 
 @app.route("/add",  methods=['GET', 'POST'])
 def add():
-    savetext = [] 
+    id = random.randint(0, 1000000000)
+    savetext
     if request.method == "POST":
         text = request.form.get('text')
-        print(text)
-        savetext.append(text)
+        savetext.update({text: id})
+        print(savetext)
     return render_template("add.html")
+
+
+
+@app.route("/mylist")
+def mylist():
+    return render_template("list.html")
+
 
 
 
