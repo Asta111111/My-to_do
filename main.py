9cbc66479d8310f220dfb350d13d44a40b363445
asta111111 <bohdanbobkov202@gmail.com>
2025-02-05 21:37:47 +0100

    add aprrove tasks

--- main.py
+++ main.py
@@ -24,23 +24,30 @@ def index():
 
 
 
-savetext = {}
+save_text = {}
+id = random.randint(0, 1000000000)
 
 @app.route("/add",  methods=['GET', 'POST'])
 def add():
     id = random.randint(0, 1000000000)
-    savetext
+    save_text
     if request.method == "POST":
         text = request.form.get('text')
-        savetext.update({text: id})
-        print(savetext)
+        save_text.update({text: [id, False]})
+        print(save_text)
     return render_template("add.html")
 
 
-
-@app.route("/mylist")
+@app.route("/mylist",  methods=['GET', 'POST'])
 def mylist():
-    return render_template("list.html")
+    if request.method == "POST":
+        if request.form.get('save') == 'SAVE':
+            for key in save_text:
+                save_text[key][1] = True
+
+
+
+    return render_template("list.html", save_text=save_text)
 
 
 
